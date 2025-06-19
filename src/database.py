142c2c51893e4c8e26acd9b517ce83c530aec88c
Andry925 <atymoshenko@thingsboard.io>
2025-06-20 00:21:04 +0300

    remove database connection, add repository to dependency

--- src/database.py
+++ src/database.py
@@ -1,27 +1,13 @@
 from typing import Annotated
 
-from decouple import config
 from fastapi import Depends
-from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy.ext.asyncio import create_async_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from repositories.in_memory_note_repository import InMemoryNoteRepository
 
-SQLALCHEMY_DATABASE_URL = config("SQLALCHEMY_DATABASE_URI")
-
-engine = create_async_engine(SQLALCHEMY_DATABASE_URL, echo=True)
-Base = declarative_base()
-async_session = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
+from repositories.note_repository import NoteRepository
 
 
-async def get_db() -> AsyncSession:
-    async with async_session() as db:
-        yield db
+def get_repo() -> NoteRepository:
+    return InMemoryNoteRepository()
 
 
-async def init_db():
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
-
-
-db_dependency = Annotated[AsyncSession, Depends(get_db)]
+repo_dependency = Annotated[NoteRepository, Depends(get_repo)]
